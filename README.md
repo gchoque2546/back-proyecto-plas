bca3520ea0049be6e57484aca02af9114ecd1d6e
gabriel <gabovchoque@gmail.com>
2024-07-24 19:37:34 -0400

    S1 Proyecto Base Laravel

---
+++ README.md
@@ -0,0 +1,26 @@
+# Comandos de GIT
+
+## Para iniciar un nuevo respositorio local
+``` bash
+git init
+``` 
+## Para clonar un nuevo respositorio remoto
+``` bash
+git clone direccion_remota
+``` 
+## Para asociar el respositorio remoto (GITHUB) con mi respositorio local
+``` bash
+git remote add origin direccion_remota
+``` 
+## LLEVAR LOS CAMBIOS AL AREA DE PREPARACION
+``` bash
+git add .
+``` 
+## AGREGAR UN COMENTARIO DE LOS CAMBIOS
+``` bash
+git commit -m "Proyecto base de laravel"
+``` 
+## SUBIR
+``` bash
+git push origin master
+``` 
---
+++ database/migrations/2024_07_24_162520_create_personas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id(); // id, ai, pk, bigInt, unsigned
+
+            $table->string("nombres", 50);
+            $table->string("apellidos", 50)->nullable();
+            $table->string("ci_nit", 20)->nullable();
+            $table->string("direccion")->nullable();
+            $table->string("telefono", 20)->nullable();
+            $table->date("fecha_nacimiento")->nullable();
+
+            //relacion 1:1
+            $table->bigInteger("user_id")->unsigned();
+            //$table->unsignedBigInteger("user_id"); otra opcion
+            $table->foreign("user_id")->references("id")->on("users");
+
+            $table->timestamps(); // created_at, update_at
+            $table->softDeletes(); // deleted_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
